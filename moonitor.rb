579bf6a93e33dcbf0fd95ef135ff9e5776be4638
Ole Riesenberg <or@oleriesenberg.com>
2009-10-18 20:29:26 +0200

    Sinatra

--- moonitor.rb
+++ moonitor.rb
@@ -1,72 +1,45 @@
-require 'atchoum'
+require 'rubygems'
+require 'sinatra'
+require 'erb'
+require 'grit'
 
-class Moonitor < Atchoum::Website
-  def layout
-    xhtml_html do
-      head do
-        title 'Moonitor :: gentoo linux emerge monitor and overlay'
-        link :rel => 'stylesheet', :href => "/base.css", :type => 'text/css'
-      end
-      body do
-        div.container! do
-          div.header! do
-            img.logo :src => "/logo.jpg"
-          end
+include Grit
 
-          div.navbar! do
-            ul.menu! do
-              li { a 'moonitor', :href => "/" }
-              li { a 'overlay', :href => "/overlay/" }
-            end
-          end
-          div.content! do
-            self << yield
-          end
- 
-          div.footer! do
-            text "&copy; 2008 Ole Riesenberg | Logo &copy; 2008 N. Kittel"
-          end
+class Package
+  attr :atom, true
+  attr :versions, true
+  attr :description, true
+
+  def initialize(atom, versions=nil, description=nil)
+    self.atom = atom
+    self.versions = versions if versions
+    self.description = description if description
+  end
+
+  def versions_string
+    self.versions.join(' | ')
+  end
+end
+
+get '/' do
+  erb :index
+end
+
+get '/overlay/' do
+  @repo = Repo.new("/home/hoodow/scm/overlay/.git")
+  
+  @packages = []
+  @repo.tree.contents.each do |category|
+    unless category.name =~ /(profiles|eclass|metadata)/
+      category.contents.each do |package|
+        versions = []
+        package.contents.each do |atom|
+          version = atom.name.scan(/-(?:\d.+)/)
+          versions << version.first.sub('-', '').sub('.ebuild', '') unless version.empty?
         end
+        @packages << Package.new("#{category.name}/#{package.name}", versions)
       end
     end
   end
-
-  def index_page
-    p.what "What is Moonitor?"
-      text "Moonitor is a dbus monitoring suite for emerge with KDE panel support, a Qt Widget and an ncurses CLI."
-
-
-    p.what "Get it"
-      text "You can clone the code directly from our git repository" 
-      code "git clone git://moonitor.org/moonitor.git"
-      code "cd moonitor"
-      code "cmake ."
-      code "make && make install"
-      
-      br
-      text "or fetch it via layman"
-      code "layman -f -o http://moonitor.org/moonitor.xml -a moonitor"
-      code "emerge sys-apps/moonitor"
-
-
-    p.what "How to contribute?"
-      text "Our Bugtracker is available at " 
-      a(:href => "http://trac.moonitor.org/") { "trac.moonitor.org" }
-  end
-
-  def overlay_page
-    p.what "What can be found in the overlay?"
-      text "The overlay currently contains the moonitor ebuilds and dependencies, some ruby and python stuff, nginx with a bunch of nice modules, games and more."
-
-
-    p.what "How to fetch it?"
-      text "You can fetch the overlay via layman"
-      code "layman -f -o http://moonitor.org/moonitor.xml -a moonitor"
-      
-      br
-      text "or do it manually"
-      code "cd /usr/portage/local"
-      code "git clone git://moonitor.org/overlay.git" 
-      code 'echo "PORTDIR_OVERLAY=\"/usr/portage/local/layman/moonitor\"" >> /etc/make.conf'
-  end
+  erb :overlay
 end
