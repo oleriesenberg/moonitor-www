20c31673af4f5a293a8c4cf927f845259c6e464c
Ole Riesenberg <or@oleriesenberg.com>
2009-10-19 13:12:39 +0200

    moved attribute extraction to Package class

--- moonitor.rb
+++ moonitor.rb
@@ -5,15 +5,27 @@ require 'grit'
 
 include Grit
 
+REPO = development? ? 
+          Repo.new("/home/hoodow/scm/overlay/.git") :
+            Repo.new("/home/git/repositories/overlay.git")
+
 class Package
   attr :atom, true
   attr :versions, true
   attr :description, true
 
-  def initialize(atom, versions=nil, description=nil)
-    self.atom = atom
+  def initialize(category, package)
+    versions = []
+    package.contents.each do |atom|
+      versions << atom.name.sub("#{package.name}-", '').sub('.ebuild', '') if atom.name =~ /.ebuild$/
+    end
+
+    blob = REPO.tree / "#{category.name}/#{package.name}/#{package.name}-#{versions.last}.ebuild"
+    grep = blob.data.scan(/^DESCRIPTION="(.*)"$/) if blob
+
+    self.atom = "#{category.name}/#{package.name}"
     self.versions = versions if versions
-    self.description = description if description
+    self.description = (blob.nil? or grep.empty?) ? nil : grep[0][0]
   end
 
   def versions_string
@@ -26,22 +38,11 @@ get '/' do
 end
 
 get '/overlay/' do
-  @repo = development? ? 
-            Repo.new("/home/hoodow/scm/overlay/.git") :
-              Repo.new("/home/git/repositories/overlay.git")
-
   @packages = []
-  @repo.tree.contents.each do |category|
+  REPO.tree.contents.each do |category|
     unless category.name =~ /(profiles|eclass|metadata)/
       category.contents.each do |package|
-        versions = []
-        package.contents.each do |atom|
-          versions << atom.name.sub("#{package.name}-", '').sub('.ebuild', '') if atom.name =~ /.ebuild$/
-        end
-        blob = @repo.tree / "#{category.name}/#{package.name}/#{package.name}-#{versions.last}.ebuild"
-        grep = blob.data.scan(/^DESCRIPTION="(.*)"$/) if blob
-        description = (blob.nil? or grep.empty?) ? nil : grep[0][0]
-        @packages << Package.new("#{category.name}/#{package.name}", versions, description)
+        @packages << Package.new(category, package)
       end
     end
   end
